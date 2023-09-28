f0e7c21c3079ea35ebe05a344b737a370963e2a3
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-09-28 17:53:36 +0800

    fix: 修正 day12 day13 顛倒問題

--- src/day13-tree-bfs-traversal/Tree.spec.js
+++ src/day13-tree-bfs-traversal/Tree.spec.js
@@ -135,3 +135,18 @@ describe.skip('Binary Tree', () => {
     expect(traversal).toEqual(expectedTraversal);
   });
 });
+
+describe.skip('Print Tree', () => {
+  it('should print the tree', () => {
+    const tree = new Tree();
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(2);
+    tree.insert(4);
+    tree.insert(6);
+    tree.insert(8);
+
+    console.log(tree.toString());
+  });
+});
